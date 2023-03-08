525ce9fdce4bade00eb034f2123b6128d2a5c668
katko23 <catalin.coseru@isa.utm.md>
2023-03-08 18:20:07 +0200

    All the files for Technical Task

---
+++ AlsoDev/tests/factories.py
@@ -0,0 +1,13 @@
+# from factory import DjangoModelFactory, Faker
+#
+# from ..models import Product
+#
+#
+# class ProductFactory(DjangoModelFactory):
+#     name = Faker('product')
+#     price = Faker('100.2')
+#     user = Faker('user1')
+#     images = Faker('1')
+#
+#     class Meta:
+#         model = Product
---
+++ AlsoDev/urls.py
@@ -0,0 +1,31 @@
+"""AlsoDev URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.urls import path
+from .views import ProductListCreateView, ProductRetrieveUpdateDestroyView, ProductImageListCreateView, \
+    ProductImageRetrieveUpdateDestroyView, ProductCharacteristicListCreateView, ProductCharacteristicRetrieveUpdateDestroyView
+
+urlpatterns = [
+    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-retrieve-update-destroy'),
+    path('product-images/', ProductImageListCreateView.as_view(), name='product-image-list-create'),
+    path('product-images/<int:pk>/', ProductImageRetrieveUpdateDestroyView.as_view(), name='product-image-retrieve-update-destroy'),
+    path('product-characteristics/', ProductCharacteristicListCreateView.as_view(), name='product-characteristic-list-create'),
+    path('product-characteristics/<int:pk>/', ProductCharacteristicRetrieveUpdateDestroyView.as_view(), name='product-characteristic-retrieve-update-destroy'),
+]
+
